04225d497af4f7845dcafa80008f90ecfdc0becd
Vultor <shawn@shawnroe.com>
2014-12-02 22:44:23 +0900

    Added images, names, and cleaned up a bit

--- js/app.js
+++ js/app.js
@@ -1,53 +1,91 @@
 $(document).ready(function(){
 	$('#image').change(function(){
-		if($('#image').val() == 'duck') {
-			$('#canvas1').drawImage({
-				source: 'img/fall-pond.jpg',
-				x: 320,
-				y: 306
-			});
-		} else if($('#image').val() == 'heart') {
-			$('#canvas1').drawImage({
-				source: 'img/korean-food.jpg',
-				x: 320,
-				y: 306
-			});
-		} else if($('#image').val() == 'puppy') {
-			$('#canvas1').drawImage({
-				source: 'img/masks.jpg',
-				x: 320,
-				y: 306
-			});
-		} else  {
-			$('#canvas1').drawImage({
-				source: 'img/korean-bow.jpg',
-				x: 320,
-				y: 306
-			});
-		} //end if
+		var image = '';
+		if($('#image').val() == 'pond') {
+			image = 'fall-pond.jpg'
+		}
+		else if($('#image').val() == 'food') {
+			image = 'guk.jpg'
+		}
+		else if($('#image').val() == 'horse') {
+			image = 'horse.jpg'
+		}
+		else if($('#image').val() == 'masks') {
+			image = 'masks.jpg'
+		}
+		else  if($('#image').val() == 'girls') {
+			image = 'girls-day.jpg'
+		}
+		else  if($('#image').val() == 'bow') {
+			image = 'korean-bow.jpg'
+		}
+		$('#canvas1').drawImage({
+			source: 'img/' + image,
+			x: 320,
+			y: 346
+		});
 	}); //end change
-	$('#create').click(function(){
-		var line1 = $('#line1').val().toUpperCase();
-		var line2 = $('#line2').val().toUpperCase();
-		$('#canvas1').drawText({
-			fillStyle: '#fff',
-			strokeStyle: '#000',
-			strokeWidth: 3,
-			x: 320,
-			y: 50,
-			fontSize: '5em',
-			fontFamily: 'Impact, sans-serif',
-			text: line1
+
+	$('#greeting').change(function() {
+		var text = '';
+		var greeting = $('#greeting').val();
+		// set the greeting text
+		if(greeting == 'lunar') {
+			text = 'Happy Lunar New Year'
+		}
+		else if(greeting == 'korean') {
+			text = 'Happy Korean New Year'
+		}
+		else if(greeting == 'roman') {
+			text = 'Saehae Bok Manhi Badeuseyo'
+		}
+		else if(greeting == 'hanguk'){
+			text = '새해 복 많이 받으세요'
+		};
+		// clear the area before drawing the text
+		$('#canvas1').clearCanvas({
+			x: 320, y: 80,
+		  	width: 640,
+		  	height: 93
 		})
+		// chaining
 		.drawText({
-			fillStyle: '#fff',
-			strokeStyle: '#000',
-			strokeWidth: 2,
-			x: 320,
-			y: 470,
-			fontSize: '3em',
-			fontFamily: 'Impact, sans-serif',
-			text: line2
-		}); //end draw text
-	}); //end click
+			fillStyle: '#EE1C25',
+			strokeStyle: '#105EB0',
+			strokeWidth: 1,
+			x: 320, y: 80,
+			fontSize: 48,
+			fontFamily: 'Zapf Chancery, Florence, cursive',
+			text: text
+		})
+	}); //end change
+
+	$('#insert').click(function() {
+		$('#canvas1').drawText({
+			fillStyle: 'darkblue',
+			x: 620, y: 580,
+			maxWidth: 600,
+			align: 'right',
+			respectAlign: true,
+			fontSize: 28,
+			fontFamily: 'Arial, sans-serif',
+			text: $('#from').val()
+		})
+		// chaining 
+		.drawText({
+			fillStyle: 'darkblue',
+			x: 20, y: 20,
+			maxWidth: 600,
+			align: 'left',
+			respectAlign: true,
+			fontSize: 28,
+			fontFamily: 'Arial, sans-serif',
+			text: 'Dear, ' + $('#to').val()
+		})
+	}); //end click insert
+
+	$('#clear').click(function() {
+		$('#canvas1').clearCanvas();
+		$('#toName').text('Dear, ');
+	}); //end click clear
 }); //end ready
